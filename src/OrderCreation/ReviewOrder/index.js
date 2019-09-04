1df77819bccf253ef38fa446c4748e319a7fbeb1
Dan <dnshaw12@gmail.com>
2019-09-04 17:12:35 -0500

    order details viewable on review order

--- src/OrderCreation/ReviewOrder/index.js
+++ src/OrderCreation/ReviewOrder/index.js
@@ -8,7 +8,7 @@ class ReviewOrder extends Component {
 		super()
 
 		this.state = {
-			order: {}
+			order: null
 		}
 	}
 
@@ -36,11 +36,103 @@ class ReviewOrder extends Component {
 
 	render(){
 
+		let dishes;
+
+		if (this.state.order) {
+
+			dishes = this.state.order.dishes.map( dish => {
+
+				let noodles = dish.extraIngredients.reduce( (acc, ingredient, idx) => {
+
+					if (ingredient.type === 'noodle') {
+
+						if (idx === 0) {
+							return ingredient.name
+						}
+
+						return acc + ', ' +ingredient.name
+					} else {
+						return acc
+					}
+
+				},'')
+
+				let proteins = dish.extraIngredients.reduce( (acc, ingredient, idx) => {
+
+					if (ingredient.type === 'protein') {
+
+						if (idx === 0) {
+							return ingredient.name
+						}
+
+						return acc + ', ' +ingredient.name
+					} else {
+						return acc
+					}
+
+				},'')
+
+				let sauces = dish.extraIngredients.reduce( (acc, ingredient, idx) => {
+
+					if (ingredient.type === 'sauce') {
+
+						if (idx === 0) {
+							return ingredient.name
+						}
+
+						return acc + ', ' +ingredient.name
+					} else {
+						return acc
+					}
+
+				},'')
+
+				const normals = dish.extraIngredients.reduce( (acc, ingredient, idx) => {
+
+					if (ingredient.type === 'normal') {
+
+						if (idx === 0) {
+							return ingredient.name
+						}
+
+						return acc + ', ' +ingredient.name
+					} else {
+						return acc
+					}
+
+				},'')
+
+				const total = dish.extraIngredients.reduce( (acc, ingredient) => {
+					return ingredient.price + acc
+				},0) + dish.menuItemId.basePrice
+
+				if (dish.menuItemId.name !== 'byon') {
+					noodles ? noodles = dish.menuItemId.noodleType.name : noodles = dish.menuItemId.noodleType.name + ', ' + noodles
+					proteins ? proteins = dish.menuItemId.protein.name : proteins = dish.menuItemId.protein.name + ', ' + proteins
+					sauces ? sauces = dish.menuItemId.sauce.name : sauces = dish.menuItemId.sauce.name + ', ' + sauces
+				}
+
+				return(
+					<Collapsible trigger={dish.menuItemId.name + '.'}>
+						<ul>
+							<li>noodz: {noodles}</li>
+							<li>protein: {proteins}</li>
+							<li>sauce: {sauces}</li>
+							<li>ingredients: {normals}</li>
+							<li>total: {total}</li>
+						</ul>
+
+					</Collapsible>
+				)
+			})
+		}
+
+
 		console.log(this.state,'on render');
 
 		return(
 			<Segment>
-				hi
+				{dishes}
 			</Segment>
 		)
 
